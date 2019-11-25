2971394c8a718c11466c94060227e45c39276e70
Firxy <zqf753951500@163.com>
2019-11-25 16:00:04 +0800

    朱乾飞提交

--- gt190806_1/src/org/gt190806_1/app/zhuqianfei/SystemTool_zhuqianfei.java
+++ gt190806_1/src/org/gt190806_1/app/zhuqianfei/SystemTool_zhuqianfei.java
@@ -2,6 +2,6 @@ package org.gt190806_1.app.zhuqianfei;
 
 public class SystemTool_zhuqianfei {
 	public void SayHi(){
-		System.out.println("ÎÒÊÇÖìÇ¬·É");
+		System.out.println("æˆ‘æ˜¯æœ±ä¹¾é£");
 	}
 }
