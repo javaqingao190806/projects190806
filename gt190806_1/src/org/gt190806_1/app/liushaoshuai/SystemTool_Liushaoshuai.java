53c366406402c404708835cecf8ab6e248b725a4
liushaoshuaizzz <lssliushaoshuai@126.com>
2019-11-25 15:25:59 +0800

    这是刘邵帅的工具类

--- gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
+++ gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
@@ -3,7 +3,7 @@ package org.gt190806_1.app.liushaoshuai;
 public class SystemTool_Liushaoshuai {
 
 	public SystemTool_Liushaoshuai() {
-		System.out.println("");
+		System.out.println("ÄãºÃ£¬ÎÒÊÇÁõÉÛË§");
 	}
 
 }
