b2be6ed2a2ed9132ffc3496990a01a9ce1803e1f
liushaoshuaizzz <lssliushaoshuai@126.com>
2019-11-25 15:33:27 +0800

    这是刘邵帅的工具类

--- gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
+++ gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
@@ -2,7 +2,7 @@ package org.gt190806_1.app.liushaoshuai;
 
 public class SystemTool_Liushaoshuai {
 
-	public SystemTool_Liushaoshuai() {
+	public void say() {
 		System.out.println("ÄãºÃ£¬ÎÒÊÇÁõÉÛË§");
 	}
 
