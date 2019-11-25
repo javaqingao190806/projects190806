c8d3acf3f187a56d6a4d750ce9c77d696c95fdca
YXG11java <Yxingguo12@163.com>
2019-11-25 17:37:04 +0800

    杨兴国上传

--- gt190806_1/src/org/gt190806_1/app/yangxingguo/SystemTool_Yangxingguo.java
+++ gt190806_1/src/org/gt190806_1/app/yangxingguo/SystemTool_Yangxingguo.java
@@ -2,6 +2,6 @@ package org.gt190806_1.app.yangxingguo;
 
 public class SystemTool_Yangxingguo {
    public static void sayHi(){
-	   System.out.println("HelloEveryone, I am 杨兴国 ");
+	   System.out.println("HelloEveryone, I'm 杨兴国 ");
    }
 }
