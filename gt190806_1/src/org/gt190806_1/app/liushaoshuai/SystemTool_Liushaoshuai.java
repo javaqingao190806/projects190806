fe9bc97da2f0010e4adf8b7e3aa6ee0b8fd44ba6
liushaoshuaizzz <lssliushaoshuai@126.com>
2019-11-25 15:23:50 +0800

    这是刘邵帅的工具类

--- gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
+++ gt190806_1/src/org/gt190806_1/app/liushaoshuai/SystemTool_Liushaoshuai.java
@@ -3,7 +3,7 @@ package org.gt190806_1.app.liushaoshuai;
 public class SystemTool_Liushaoshuai {
 
 	public SystemTool_Liushaoshuai() {
-		// TODO Auto-generated constructor stub
+		System.out.println("");
 	}
 
 }
