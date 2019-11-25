c3e8169dd8f768f3df25758a78c4e59c32bca7c0
zyb17863942950 <zyb17863942950@163.com>
2019-11-25 15:48:41 +0800

    张云宝提交

--- gt190806_1/src/org/gt190806_1/app/zhangyunbao/System_tool_zhangyunbao.java
+++
@@ -1,8 +0,0 @@
-package org.gt190806_1.app.zhangyunbao;
-
-public class System_tool_zhangyunbao {
-	public void Sayhi(){
-		
-		System.out.println("nihao");
-	}
-}
---
+++ gt190806_1/src/org/gt190806_1app/zhangyunbao/System_tool_zhangyunbao.java
@@ -0,0 +1,7 @@
+package org.gt190806_1app.zhangyunbao;
+
+public class System_tool_zhangyunbao {
+     public static void main(String[] args) {
+		System.out.println("你好，张云宝");
+	}
+}
