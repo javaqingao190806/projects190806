c7ddaca094a89bb5a2f69fcf6fa6abe1c8d62f0c
ren981007 <ren981007@126.com>
2019-11-25 16:40:28 +0800

    renjunqing

--- gt190806_1/src/org/gt190806_1/app/renjunqing/SystemTool_RenJunQing.java
+++ gt190806_1/src/org/gt190806_1/app/renjunqing/SystemTool_RenJunQing.java
@@ -2,7 +2,7 @@ package org.gt190806_1.app.renjunqing;
 
 public class SystemTool_RenJunQing {
 	public static void main(String[] args) {
-		System.out.println("你好，我是任俊清");
+		System.out.println("你好，我是任俊清，你好");
 	}
 
 }
