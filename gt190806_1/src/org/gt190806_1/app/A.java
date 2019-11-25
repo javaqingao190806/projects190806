7f2259bccb4359530cb4b5d33344f73f9900df85
ren981007 <ren981007@126.com>
2019-11-25 15:14:57 +0800

    任俊清

--- gt190806_1/src/org/gt190806_1/app/A.java
+++ gt190806_1/src/org/gt190806_1/app/A.java
@@ -3,6 +3,6 @@ package org.gt190806_1.app;
 public class A {
 
 	public void sayHi(){
-		System.out.println("Hello 你好,天气凉了");
+		System.out.println("Hello 你好,天气凉了,");
 	}
 }
---
+++ gt190806_1/src/org/gt190806_1/app/renjunqing/SystemTool_renjunqing.java
@@ -0,0 +1,10 @@
+package org.gt190806_1.app.renjunqing;
+
+import sun.applet.Main;
+
+public class SystemTool_renjunqing {
+public static void main(String[] args) {
+	System.out.println("你好，我是任俊清");
+}
+
+}
